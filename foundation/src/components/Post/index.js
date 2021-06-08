4e4b82a55ef0ae5eed380b5e4f2e722a5f38cabc
Matthew Jiao <jiaomatthew222@gmail.com>
2021-06-08 01:48:21 -0400

    post component

--- foundation/src/components/Post/index.js
+++ foundation/src/components/Post/index.js
@@ -20,9 +20,20 @@ const Post = (props) => {
 
         />
 
+        <Text style = {styles.bedrooms}>1 bed 1 bedroom</Text>
+        <Text style = {styles.description} numberOfLines = {2}>What is Lorem Ipsum Lorem Ipsum is simply dummy 
+            text of the printing and typesetting industry Lorem Ipsum has been the industry's 
+            standard dummy text ever since the 1500s
+        </Text>
 
-     
-     
+        <Text style = {styles.prices}>
+            <Text style = {styles.oldPrice}>$36 </Text>
+            <Text style = {styles.price}> $30 </Text>
+            / night
+        </Text>
+
+        <Text style = {styles.totalPrice}>$230 total</Text>
+
 
 
 
--- foundation/src/components/Post/styles.js
+++ foundation/src/components/Post/styles.js
@@ -3,7 +3,6 @@ import {StyleSheet} from 'react-native'
 const styles = StyleSheet.create({
     container: {
         margin: 20,
-        alignItems: 'center'
     },
     image: {
         width: '100%',
@@ -11,6 +10,30 @@ const styles = StyleSheet.create({
         aspectRatio: 3/2,
         resizeMode: 'cover',
         borderRadius: 10,
+    },
+    prices: {
+        fontSize: 18,
+        marginVertical: 10
+    },
+    oldPrice: {
+        color: '#5b5b5b',
+        textDecorationLine: 'line-through',
+    },
+    price: {
+        fontWeight: 'bold'
+    },
+    totalPrice: {
+        color: '#5b5b5b',
+        textDecorationLine: 'underline',
+
+    },
+    description: {
+        fontSize: 18,
+        lineHeight: 26
+    }, 
+    bedrooms: {
+        marginVertical: 10,
+        color: '#5b5b5b'
     }
 
 })
