3a4a3c8b9adf588568f1fc624f9452af6cd96a93
Matthew Jiao <jiaomatthew222@gmail.com>
2021-06-16 02:11:53 -0400

    post screen

---
+++ foundation/src/screens/PostScreen/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { View, Text} from 'react-native';
+
+
+
+const PostScreen = (props) => {
+       
+
+    return (
+        <View>
+            <Text>Hi</Text>
+        </View>
+    )
+
+}
+
+  
+
+  
+
+export default PostScreen
